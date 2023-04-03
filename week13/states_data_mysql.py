1694823ac0c0489473454b5334c33dd8f62b3260
Osabuohien Randalf Ehigiator <107191688+guyrandalf@users.noreply.github.com>
2023-04-03 21:06:10 +0100

    No commit

--- week13/states_data_mysql.py
+++ week13/states_data_mysql.py
@@ -13,25 +13,42 @@ if mydb:
     # Create a cursor object
     mycursor = mydb.cursor()
 
+
 def main():
     try:
-        states = get_states
-
-        for row in states():
+        all_states = get_states
+        # one_state = get_a_state
+        counter = 0
+        for row in all_states():
             # Print the results
-            print(f'State: {row[1].upper()} ')
+            counter = counter + 1
+            print(f'({counter}) State: {row[1].upper()} ')
             print(f'Local Govt Areas: {row[2]} ')
+            print()
+
+        # for row in one_state():
+        #     # Print the results
+        #     print(f'State: {row[1].upper()} ')
+        #     print(f'Local Govt Areas: {row[2]} ')
+        #     print()
 
     except:
         print('Connection not established')
 
+    finally:
+        if mydb.is_connected():
+            mycursor.close()
+            mydb.close()
+
+
 def get_states():
-    """ Queries the database and gets list of all states from the 'states' table """
-    
+    """ Queries the database and gets list of all states and local government areas from the table """
     mycursor.execute(
-            "SELECT * FROM states")   
+        "SELECT * FROM states")
     get_all_states = mycursor.fetchall()
+
     return get_all_states
+
 
 if __name__ == "__main__":
     main()
